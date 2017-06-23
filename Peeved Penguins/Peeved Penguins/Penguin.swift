a90266499112b9c70adc79b5e2dbaed801224be7
Marshall Cain <mcain251@gmail.com>
2017-06-23 11:28:13 -0700

    Add Penguin class

---
+++ Peeved Penguins/Peeved Penguins/Penguin.swift
@@ -0,0 +1,31 @@
+//
+//  Penguin.swift
+//  Peeved Penguins
+//
+//  Created by Marshall Cain on 6/23/17.
+//  Copyright © 2017 Marshall Cain. All rights reserved.
+//
+
+import SpriteKit
+
+class Penguin: SKSpriteNode {
+    
+    init() {
+        
+        // Make Penguin texture and initialize
+        let texture = SKTexture(imageNamed: "flyingpenguin")
+        let color = UIColor.clear
+        let size = texture.size()
+        super.init(texture: texture, color: color, size: size)
+        
+        // Set physics properties
+        physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+        physicsBody?.categoryBitMask = 1
+        physicsBody?.friction = 0.6
+        physicsBody?.mass = 0.5
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
