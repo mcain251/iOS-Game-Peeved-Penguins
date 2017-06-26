820b02de4178c0bdb9be884b969e60d0c8b8ee47
Marshall Cain <mcain251@gmail.com>
2017-06-26 10:52:41 -0700

    Add camera and restart button

--- Peeved Penguins/Peeved Penguins/GameScene.swift
+++ Peeved Penguins/Peeved Penguins/GameScene.swift
@@ -8,16 +8,49 @@
 
 import SpriteKit
 
+// Clamp function
+func clamp<T: Comparable>(value: T, lower: T, upper: T) -> T {
+    return min(max(value, lower), upper)
+}
+
 class GameScene: SKScene {
     
     // Game objects
     var catapultArm: SKSpriteNode!
+    
+    // Camera objects
+    var cameraNode: SKCameraNode!
+    var cameraTarget: SKSpriteNode?
+    
+    // Buttons
+    var restartButton: MSButtonNode!
     
     // Set up scene
     override func didMove(to view: SKView) {
         
         // Set reference to catapultArm
         catapultArm = childNode(withName: "catapultArm") as! SKSpriteNode
+        
+        // Set reference to cameraNode and make it the camera
+        cameraNode = childNode(withName: "cameraNode") as! SKCameraNode
+        camera = cameraNode
+        
+        // Set reference to restart button
+        restartButton = childNode(withName: "//restartButton") as! MSButtonNode
+        
+        restartButton.selectedHandler = {
+            // Creates GameScene
+            guard let scene = GameScene.level(1) else {
+                print ("Could not load GameScene level 1")
+                return
+            }
+            
+            // Ensure correct aspect mode
+            scene.scaleMode = .aspectFill
+            
+            // Start GameScene
+            view.presentScene(scene)
+        }
     }
     
     // Called when screen is first touched
@@ -36,11 +69,16 @@ class GameScene: SKScene {
         // Apply impulse to penguin
         let launchImpulse = CGVector(dx: 400, dy: 0)
         penguin.physicsBody?.applyImpulse(launchImpulse)
+        
+        // Set camera to follow penguin
+        cameraTarget = penguin
     }
     
     // Called in each frame
     override func update(_ currentTime: CFTimeInterval) {
         
+        // Follows penguin
+        moveCamera()
     }
     
     // Returns a specific level
@@ -50,5 +88,15 @@ class GameScene: SKScene {
         }
         scene.scaleMode = .aspectFill
         return scene
+    }
+    
+    // Tracks the cameraTarget with the camera
+    func moveCamera() {
+        guard let cameraTarget = cameraTarget else {
+            return
+        }
+        let targetX = cameraTarget.position.x
+        let x = clamp(value: targetX, lower: 0, upper: 392)
+        cameraNode.position.x = x
     }
 }
